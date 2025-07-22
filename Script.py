3f415bbe4680de15832f4f0bf036c8cd585317c0
saurabh6055 <153442157+saurabh6055@users.noreply.github.com>
2025-07-22 09:07:13 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -118,14 +118,14 @@ Added By - {}
 FɪʟᴇSɪᴢᴇ : {file_size}
 
 ╔═  ᴊᴏɪɴ ᴡɪᴛʜ ᴜs   ═╗
- Jᴏɪɴ :- [MAIN CHANNEL](https://t.me/mn_movies2)
- Jᴏɪɴ :- [Movie Group 1](https://t.me/mn_movies3)
- Jᴏɪɴ :- [Movie Group 2](https://t.me/malayalam_movies_group2)
- Jᴏɪɴ :- [Movie Group 3](https://t.me/Netflix_Group3)
- Jᴏɪɴ :- [Movie Group 4](https://t.me/cinima_theerthadana_kendram)
- Jᴏɪɴ :- [Movie Group 5](https://t.me/malayalam_movies_nbot)
- Jᴏɪɴ :- [Movie Group 6](https://t.me/seriesgroups)
- Jᴏɪɴ :- [Movie Group 7](https://t.me/New_indian_cinemas)
+ Jᴏɪɴ :- [MAIN CHANNEL](https://t.me/erean_status)
+ Jᴏɪɴ :- [Movie Group 1](https://t.me/erean_status)
+ Jᴏɪɴ :- [Movie Group 2](https://t.me/erean_status)
+ Jᴏɪɴ :- [Movie Group 3](https://t.me/erean_status)
+ Jᴏɪɴ :- [Movie Group 4](https://t.me/erean_status)
+ Jᴏɪɴ :- [Movie Group 5](https://t.me/erean_status)
+ Jᴏɪɴ :- [Movie Group 6](https://t.me/erean_status)
+ Jᴏɪɴ :- [Movie Group 7](https://t.me/erean_status)
 ╚═  ᴊᴏɪɴ ᴡɪᴛʜ ᴜs    ═╝
 
 ⚠️ <b>This file will be deleted from here within 1 minute as it has copyright ... !!!</b>
